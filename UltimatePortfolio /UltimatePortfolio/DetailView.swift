46c0f05218528cd44d1d8e3c4c97dcafa99f38dd
seymen-elmas <seymennelmas@gmail.com>
2024-09-25 10:52:40 +0300

    Ultimate Portfolio upgraded

---
+++ UltimatePortfolio /UltimatePortfolio/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  UltimatePortfolio
+//
+//  Created by Seymen Nadir Elmas on 25.09.2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ UltimatePortfolio /UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,31 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Seymen Nadir Elmas on 23.09.2024.
+//
+
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController = DataController()
+
+    var body: some Scene {
+        WindowGroup {
+            NavigationSplitView {
+                SidebarView()
+            } content: {
+                ContentView()
+                  
+            } detail: {
+                DetailView()
+            }
+            .environment(\.managedObjectContext, dataController.container.viewContext)
+            .environmentObject(dataController)
+
+          
+        }
+    }
+}
