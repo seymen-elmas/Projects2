e8d814104c0267e8d2bae08f04ad40445088b92d
seymen-elmas <seymennelmas@gmail.com>
2024-10-05 13:47:01 +0300

    Ultimate Portfolio Upgraded Testing Added

---
+++ UltimatePortfolio/UltimatePortfolio /UltimatePortfolioTests/UltimatePortfolioTests.swift
@@ -0,0 +1,20 @@
+//
+//  UltimatePortfolioTests.swift
+//  UltimatePortfolioTests
+//
+//  Created by Seymen Nadir Elmas on 5.10.2024.
+//
+
+import CoreData
+import XCTest
+@testable import UltimatePortfolio
+
+class BaseTestCase: XCTestCase {
+    var dataController: DataController!
+    var managedObjectContext: NSManagedObjectContext!
+
+    override func setUpWithError() throws {
+        dataController = DataController(inMemory: true)
+        managedObjectContext = dataController.container.viewContext
+    }
+}
