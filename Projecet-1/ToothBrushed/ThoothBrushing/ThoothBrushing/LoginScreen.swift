3b32be005e3fe094be4ad205b595a363f72c85ac
seymen-elmas <seymennelmas@gmail.com>
2024-10-28 14:59:06 +0300

    tooth brush app editing firebase

--- Projecet-1/ToothBrushed/ThoothBrushing/ThoothBrushing/LoginScreen.swift
+++ Projecet-1/ToothBrushed/ThoothBrushing/ThoothBrushing/LoginScreen.swift
@@ -6,12 +6,72 @@
 //
 
 import SwiftUI
-
+import FirebaseAuth
 struct LoginScreen: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    @State private var email = ""
+        @State private var password = ""
+        @State private var errorMessage = ""
+
+        var body: some View {
+            VStack(spacing: 20) {
+                Text("Welcome Thooth Brushing")
+                TextField("Email", text: $email)
+                    .keyboardType(.emailAddress)
+                    .autocapitalization(.none)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8)
+
+                Button("Sign Up") {
+                    signUp()
+                }
+                .padding()
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+
+                Button("Login") {
+                    login()
+                }
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+
+                if !errorMessage.isEmpty {
+                    Text(errorMessage)
+                        .foregroundColor(.red)
+                }
+            }
+            .padding()
+        }
+
+        private func signUp() {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    errorMessage = error.localizedDescription
+                } else {
+                    errorMessage = "User created successfully!"
+                }
+            }
+        }
+
+        private func login() {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    errorMessage = error.localizedDescription
+                } else {
+                    errorMessage = "Login successful!"
+                }
+            }
+        }
     }
-}
+
 
 #Preview {
     LoginScreen()
